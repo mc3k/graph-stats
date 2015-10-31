391b240ff2c621edaf00f3c33e6b1be01b227f27
mc3k <mc3k@users.noreply.github.com>
2015-10-31 02:08:10 +0000

    Update plottemp.sh

--- plot-temp/plottemp.sh
+++ plot-temp/plottemp.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+
+python3 temp.py 
+
+echo "
+set terminal png size 900, 300
+set xdata time
+set xrange [ time(0) - 86400 : time(0) ]    # 86400 sec = 1 day
+set timefmt '%Y-%m-%d,%H:%M:%S'
+set format x '%H:%M'
+set output '/home/pi/bin/plottemp/tempplot.png'
+plot '/home/pi/bin/plottemp/tempdata.dat' using 1:2 with lines title 'CPU Temp deg C'" | gnuplot
