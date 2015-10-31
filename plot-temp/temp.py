9dc82edf953a59438c073bc5a8754de95c1f6461
mc3k <mc3k@users.noreply.github.com>
2015-10-31 02:09:26 +0000

    Update temp.py

--- plot-temp/temp.py
+++ plot-temp/temp.py
@@ -1,1 +1,18 @@
 
+#!/usr/bin/env python
+
+import sys, os, logging, urllib, datetime
+
+def fetchtemp():
+	cmd = '/opt/vc/bin/vcgencmd measure_temp'
+	line = os.popen(cmd).readline().strip()
+	output = line.split('=')[1].split("'")[0]#+' C'
+	return output 
+
+format = "%Y-%m-%d,%H:%M:%S" 
+today = datetime.datetime.today()
+s = today.strftime(format)
+output = s+' '+fetchtemp()+'\n'
+
+with open('/home/pi/bin/plot-temp/temp.dat', 'a') as f:
+	f.write(output)
